00ec7383bc86882f3de29286e35e443f091868b4
Ankit Sharma <ankitsharma2615@gmail.com>
2026-01-26 21:55:47 +0530

    chore: fix new email verification issue

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -9,10 +9,19 @@ import { sendWelcomeEmail } from "../emails/welcome-email";
 
 export const auth = betterAuth({
   user: {
+    changeEmail: {
+      enabled: true,
+      sendChangeEmailVerification: async ({ user, url, newEmail }) => {
+        await sendEmailVerification({
+          user: { ...user, email: newEmail },
+          url,
+        });
+      },
+    },
     additionalFields: {
       favoriteNumber: {
         type: "number",
-        optional: true,
+        required: true,
       },
     },
   },
