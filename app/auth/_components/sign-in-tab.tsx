c6a21cb341a460207f3a0664505ec8fedcc8fcfb
Ankit Sharma <ankitsharma2615@gmail.com>
2026-02-04 23:36:24 +0530

    feat: add passkey

--- app/auth/_components/sign-in-tab.tsx
+++ app/auth/_components/sign-in-tab.tsx
@@ -1,22 +1,24 @@
 "use client";
 
-import z from "zod";
-import { toast } from "sonner";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
 import { Button } from "@/components/ui/button";
 import { LoadingSwap } from "@/components/ui/loading-swap";
-import { PasswordInput } from "@/components/ui/password-input";
 import { authClient } from "@/lib/auth/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { PasskeyButton } from "./passkey-button";
 
 const signInSchema = z.object({
   email: z.email().min(1),
@@ -33,7 +35,6 @@ export function SignInTab({
   openForgotPassword: () => void;
 }) {
   const router = useRouter();
-
   const form = useForm<SignInForm>({
     resolver: zodResolver(signInSchema),
     defaultValues: {
@@ -46,18 +47,14 @@ export function SignInTab({
 
   async function handleSignIn(data: SignInForm) {
     await authClient.signIn.email(
-      {
-        ...data,
-        callbackURL: "/",
-      },
+      { ...data, callbackURL: "/" },
       {
         onError: (error) => {
           if (error.error.code === "EMAIL_NOT_VERIFIED") {
             openEmailVerificationTab(data.email);
           }
-          toast.error(error.error.message || "Something went wrong");
+          toast.error(error.error.message || "Failed to sign in");
         },
-
         onSuccess: () => {
           router.push("/");
         },
@@ -66,53 +63,61 @@ export function SignInTab({
   }
 
   return (
-    <Form {...form}>
-      <form className="space-y-4" onSubmit={form.handleSubmit(handleSignIn)}>
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+    <div className="space-y-4">
+      <Form {...form}>
+        <form className="space-y-4" onSubmit={form.handleSubmit(handleSignIn)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    autoComplete="email webauthn"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <div className="flex justify-between items-center">
-                <FormLabel>Password</FormLabel>
-                <Button
-                  onClick={openForgotPassword}
-                  type="button"
-                  variant="link"
-                  size="sm"
-                  className="text-sm font-normal underline"
-                >
-                  Forgot Password?
-                </Button>
-              </div>
-              <FormControl>
-                <PasswordInput {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex justify-between items-center">
+                  <FormLabel>Password</FormLabel>
+                  <Button
+                    onClick={openForgotPassword}
+                    type="button"
+                    variant="link"
+                    size="sm"
+                    className="text-sm font-normal underline"
+                  >
+                    Forgot password?
+                  </Button>
+                </div>
+                <FormControl>
+                  <PasswordInput
+                    autoComplete="current-password webauthn"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Button
-          type="submit"
-          disabled={isSubmitting}
-          className="w-full cursor-pointer"
-        >
-          <LoadingSwap isLoading={isSubmitting}>Sign In</LoadingSwap>
-        </Button>
-      </form>
-    </Form>
+          <Button type="submit" disabled={isSubmitting} className="w-full">
+            <LoadingSwap isLoading={isSubmitting}>Sign In</LoadingSwap>
+          </Button>
+        </form>
+      </Form>
+      <PasskeyButton />
+    </div>
   );
 }
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -8,6 +8,7 @@ import { createAuthMiddleware } from "better-auth/api";
 import { sendWelcomeEmail } from "../emails/welcome-email";
 import { sendDeleteAccountVerificationEmail } from "../emails/delete-account-verification";
 import { twoFactor } from "better-auth/plugins";
+import { passkey } from "@better-auth/passkey";
 
 export const auth = betterAuth({
   user: {
@@ -73,7 +74,7 @@ export const auth = betterAuth({
       maxAge: 60, // 1 minutes
     },
   },
-  plugins: [nextCookies(), twoFactor()],
+  plugins: [nextCookies(), twoFactor(), passkey()],
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
