0b862f9a0b96149c1be30acf33d02a890c2d987c
Ankit Sharma <ankitsharma2615@gmail.com>
2026-02-05 20:24:32 +0530

    chore: comment useEffect for passkey

--- app/auth/_components/passkey-button.tsx
+++ app/auth/_components/passkey-button.tsx
@@ -3,23 +3,23 @@
 import { BetterAuthActionButton } from "@/components/auth/better-auth-action-button";
 import { authClient } from "@/lib/auth/auth-client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 export function PasskeyButton() {
   const router = useRouter();
   const { refetch } = authClient.useSession();
 
-  useEffect(() => {
-    authClient.signIn.passkey(
-      { autoFill: true },
-      {
-        onSuccess() {
-          refetch();
-          router.push("/");
-        },
-      },
-    );
-  }, [router, refetch]);
+  // useEffect(() => {
+  //   authClient.signIn.passkey(
+  //     { autoFill: true },
+  //     {
+  //       onSuccess() {
+  //         refetch();
+  //         router.push("/");
+  //       },
+  //     },
+  //   );
+  // }, [router, refetch]);
 
   return (
     <BetterAuthActionButton
