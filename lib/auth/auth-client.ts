df2628d663068cd3d69764bf28f756c533a98e50
Ankit Sharma <ankitsharma2615@gmail.com>
2026-02-08 22:31:16 +0530

    wip: Polar Plugin

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -8,6 +8,7 @@ import {
 } from "better-auth/client/plugins";
 import { passkeyClient } from "@better-auth/passkey/client";
 import { ac, admin, user } from "@/components/auth/permissions";
+import { polarClient } from "@polar-sh/better-auth";
 
 export const authClient = createAuthClient({
   plugins: [
@@ -26,5 +27,6 @@ export const authClient = createAuthClient({
       },
     }),
     organizationClient(),
+    polarClient(),
   ],
 });
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,22 +1,45 @@
-import { db } from "@/drizzle/db";
+// Better-Auth
 import { betterAuth } from "better-auth";
+import { passkey } from "@better-auth/passkey";
 import { nextCookies } from "better-auth/next-js";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { sendPasswordResetEmail } from "../emails/password-reset-email";
-import { sendEmailVerification } from "../emails/email-verification";
 import { createAuthMiddleware } from "better-auth/api";
-import { sendWelcomeEmail } from "../emails/welcome-email";
-import { sendDeleteAccountVerificationEmail } from "../emails/delete-account-verification";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import {
   admin as adminPlugin,
   organization,
   twoFactor,
 } from "better-auth/plugins";
-import { passkey } from "@better-auth/passkey";
-import { ac, admin, user } from "@/components/auth/permissions";
-import { sendOrganizationInviteEmail } from "../emails/organization-invite-email";
-import { member } from "@/drizzle/schema";
+
+// Drizzle
+import { db } from "@/drizzle/db";
 import { desc, eq } from "drizzle-orm";
+import { member } from "@/drizzle/schema";
+
+// Polar
+import { Polar } from "@polar-sh/sdk";
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  webhooks,
+} from "@polar-sh/better-auth";
+
+// Emails
+import { sendWelcomeEmail } from "../emails/welcome-email";
+import { sendEmailVerification } from "../emails/email-verification";
+import { sendPasswordResetEmail } from "../emails/password-reset-email";
+import { sendOrganizationInviteEmail } from "../emails/organization-invite-email";
+import { sendDeleteAccountVerificationEmail } from "../emails/delete-account-verification";
+import { ac, admin, user } from "@/components/auth/permissions";
+
+const polarClient = new Polar({
+  accessToken: process.env.POLAR_ACCESS_TOKEN,
+  // Use 'sandbox' if you're using the Polar Sandbox environment
+  // Remember that access tokens, products, etc. are completely separated between environments.
+  // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
+  server: "sandbox",
+});
 
 export const auth = betterAuth({
   appName: "Better Auth Guide",
@@ -108,6 +131,28 @@ export const auth = betterAuth({
           email,
         });
       },
+    }),
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "575ec29d-5d7b-4c72-82dc-39b5bc428695",
+              slug: "pro",
+            },
+            {
+              productId: "3b70eae9-8f5a-4013-b48a-8f781dbfd92c",
+              slug: "pro-max",
+            },
+          ],
+          successUrl: "/success?checkout_id={CHECKOUT_ID}",
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        usage(),
+      ],
     }),
   ],
   database: drizzleAdapter(db, {
